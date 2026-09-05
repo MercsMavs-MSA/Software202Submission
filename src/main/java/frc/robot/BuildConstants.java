c250228fb0004805daa3bf3b915100b7a193a494
Arnav Sutar <arnav.s.sutar@gmail.com>
2026-09-05 14:36:00 -0500

    Advantage kit skeleton project

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Software202Submission";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "146e00268f1da1b28b2207f67fefac50ae988771";
+  public static final String GIT_DATE = "2026-08-29 17:35:35 EDT";
+  public static final String GIT_BRANCH = "ArnavS";
+  public static final String BUILD_DATE = "2026-09-05 15:22:01 EDT";
+  public static final long BUILD_UNIX_TIME = 1788636121460L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
